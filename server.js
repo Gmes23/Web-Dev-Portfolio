dcb0d03e265ff633fac0aa0df6decee6daff6cd3
Gmes23 <gmesda23@gmail.com>
2017-01-05 22:09:55 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var routes = require('./controllers/controller.js');
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 app.use(methodOverride('_method'));
